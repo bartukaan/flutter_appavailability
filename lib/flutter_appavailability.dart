e227a0048683b496a00d7049b5baf3cc16d0c42b
Bartu Kaan Pideci <bartu.pideci@ginifinance.com>
2021-11-26 15:35:48 +0300

    android embedding v2

--- lib/flutter_appavailability.dart
+++ lib/flutter_appavailability.dart
@@ -24,18 +24,18 @@ class AppAvailability {
   static Future<Map<String, String>> checkAvailability(String uri) async {
     Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent('uri', () => uri);
-
     if (Platform.isAndroid) {
-      Map<dynamic, dynamic> app = await _channel.invokeMethod("checkAvailability", args);
+      Map<dynamic, dynamic> app =
+          await _channel.invokeMethod("checkAvailability", args);
       return {
         "app_name": app["app_name"],
         "package_name": app["package_name"],
         "versionCode": app["versionCode"],
         "version_name": app["version_name"]
       };
-    }
-    else if (Platform.isIOS) {
-      bool appAvailable = await _channel.invokeMethod("checkAvailability", args);
+    } else if (Platform.isIOS) {
+      bool appAvailable =
+          await _channel.invokeMethod("checkAvailability", args);
       if (!appAvailable) {
         throw PlatformException(code: "", message: "App not found $uri");
       }
@@ -46,18 +46,16 @@ class AppAvailability {
         "version_name": ""
       };
     }
-
-    return null;
+    return Future.value({});
   }
-
   /// Only for **Android**.
   ///
   /// Get the list of all installed apps, where
   /// each app has a form like [checkAvailability()].
   static Future<List<Map<String, String>>> getInstalledApps() async {
-    List<dynamic> apps = await _channel.invokeMethod("getInstalledApps");
+    List<dynamic>? apps = await _channel.invokeMethod("getInstalledApps");
     if (apps != null && apps is List) {
-      List<Map<String, String>> list = new List();
+      List<Map<String, String>> list = [];
       for (var app in apps) {
         if (app is Map) {
           list.add({
@@ -68,12 +66,10 @@ class AppAvailability {
           });
         }
       }
-
       return list;
     }
-    return new List(0);
+    return List.empty();
   }
-
   /// Only for **Android**.
   ///
   /// Check if the app is enabled or not with the given [uri] scheme.
@@ -84,7 +80,6 @@ class AppAvailability {
     args.putIfAbsent('uri', () => uri);
     return await _channel.invokeMethod("isAppEnabled", args);
   }
-
   /// Launch an app with the given [uri] scheme if it exists.
   ///
   /// If the app app isn't found, then a [PlatformException] is thrown.
@@ -93,14 +88,11 @@ class AppAvailability {
     args.putIfAbsent('uri', () => uri);
     if (Platform.isAndroid) {
       await _channel.invokeMethod("launchApp", args);
-    }
-    else if (Platform.isIOS) {
+    } else if (Platform.isIOS) {
       bool appAvailable = await _channel.invokeMethod("launchApp", args);
       if (!appAvailable) {
         throw PlatformException(code: "", message: "App not found $uri");
       }
     }
-
   }
-
 }
