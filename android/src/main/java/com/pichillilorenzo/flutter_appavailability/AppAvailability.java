f24cb4f0c82331600bf494aa056c85ca4919d351
Bartu Kaan Pideci <bartu.pideci@ginifinance.com>
2021-11-26 15:24:23 +0300

    android embedding v2

--- android/src/main/java/com/pichillilorenzo/flutter_appavailability/AppAvailability.java
+++ android/src/main/java/com/pichillilorenzo/flutter_appavailability/AppAvailability.java
@@ -1,41 +1,46 @@
 package com.pichillilorenzo.flutter_appavailability;
 
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageInfo;
-import android.content.pm.ApplicationInfo;
-import android.os.Build;
-import android.annotation.TargetApi;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
-/** AppAvailability */
-public class AppAvailability implements MethodCallHandler {
+/**
+ * AppAvailability
+ */
+public class AppAvailability implements FlutterPlugin, MethodCallHandler {
 
-  private final Activity activity;
-  private final Registrar registrar;
+  private Context applicationContext;
+  private MethodChannel channel;
 
-  public AppAvailability(Registrar registrar, Activity activity) {
-    this.registrar = registrar;
-    this.activity = activity;
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), "com.pichillilorenzo/flutter_appavailability");
+    channel.setMethodCallHandler(this);
+    applicationContext = binding.getApplicationContext();
   }
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.pichillilorenzo/flutter_appavailability");
-    channel.setMethodCallHandler(new AppAvailability(registrar, registrar.activity()));
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
+
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
@@ -43,18 +48,18 @@ public class AppAvailability implements MethodCallHandler {
     switch (call.method) {
       case "checkAvailability":
         uriSchema = call.argument("uri").toString();
-        this.checkAvailability( uriSchema, result );
+        this.checkAvailability(uriSchema, result);
         break;
       case "getInstalledApps":
         result.success(getInstalledApps());
         break;
       case "isAppEnabled":
         uriSchema = call.argument("uri").toString();
-        this.isAppEnabled( uriSchema, result );
+        this.isAppEnabled(uriSchema, result);
         break;
       case "launchApp":
         uriSchema = call.argument("uri").toString();
-        this.launchApp(uriSchema , result);
+        this.launchApp(uriSchema, result);
         break;
       default:
         result.notImplemented();
@@ -64,7 +69,7 @@ public class AppAvailability implements MethodCallHandler {
   private void checkAvailability(String uri, Result result) {
     PackageInfo info = getAppPackageInfo(uri);
 
-    if(info != null) {
+    if (info != null) {
       result.success(this.convertPackageInfoToJson(info));
       return;
     }
@@ -72,7 +77,7 @@ public class AppAvailability implements MethodCallHandler {
   }
 
   private List<Map<String, Object>> getInstalledApps() {
-    PackageManager packageManager = registrar.context().getPackageManager();
+    PackageManager packageManager = applicationContext.getPackageManager();
     List<PackageInfo> apps = packageManager.getInstalledPackages(0);
     List<Map<String, Object>> installedApps = new ArrayList<>(apps.size());
     int systemAppMask = ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP;
@@ -90,13 +95,11 @@ public class AppAvailability implements MethodCallHandler {
   }
 
   private PackageInfo getAppPackageInfo(String uri) {
-    Context ctx = activity.getApplicationContext();
-    final PackageManager pm = ctx.getPackageManager();
+    final PackageManager pm = applicationContext.getPackageManager();
 
     try {
       return pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-    }
-    catch(PackageManager.NameNotFoundException e) {
+    } catch (PackageManager.NameNotFoundException e) {
 
     }
 
@@ -105,7 +108,7 @@ public class AppAvailability implements MethodCallHandler {
 
   private Map<String, Object> convertPackageInfoToJson(PackageInfo info) {
     Map<String, Object> map = new HashMap<>();
-    map.put("app_name", info.applicationInfo.loadLabel(registrar.context().getPackageManager()).toString());
+    map.put("app_name", info.applicationInfo.loadLabel(applicationContext.getPackageManager()).toString());
     map.put("package_name", info.packageName);
     map.put("version_code", String.valueOf(info.versionCode));
     map.put("version_name", info.versionName);
@@ -115,7 +118,7 @@ public class AppAvailability implements MethodCallHandler {
   private void isAppEnabled(String packageName, Result result) {
     boolean appStatus = false;
     try {
-      ApplicationInfo ai = registrar.context().getPackageManager().getApplicationInfo(packageName, 0);
+      ApplicationInfo ai = applicationContext.getPackageManager().getApplicationInfo(packageName, 0);
       if (ai != null) {
         appStatus = ai.enabled;
       }
@@ -130,11 +133,11 @@ public class AppAvailability implements MethodCallHandler {
   private void launchApp(String packageName, Result result) {
     PackageInfo info = getAppPackageInfo(packageName);
 
-    if(info != null) {
-      Intent launchIntent = registrar.context().getPackageManager().getLaunchIntentForPackage(packageName);
+    if (info != null) {
+      Intent launchIntent = applicationContext.getPackageManager().getLaunchIntentForPackage(packageName);
       if (launchIntent != null) {
         launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        registrar.context().startActivity(launchIntent);
+        applicationContext.startActivity(launchIntent);
         result.success(null);
         return;
       }
